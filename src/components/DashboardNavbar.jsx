947796c52e1d94431b770a7d399ad0053758875e
iniubongisemin <eugeneinie@gmail.com>
2024-03-15 13:08:15 +0100

    rerouted to home page

--- src/components/DashboardNavbar.jsx
+++ src/components/DashboardNavbar.jsx
@@ -12,7 +12,7 @@ const DashboardNavbar = () => {
   return (
     <div>
       <div className='relative left-0 right-0 top-0 flex justify-around items-center px-[30px] pt-6 bg-white z-50 h-24'>
-            <img src={logo} alt="" className="w-[100px] cursor-pointer" onClick={() => navigate('/onboarding')} />
+            <img src={logo} alt="" className="w-[100px] cursor-pointer" onClick={() => navigate('/')} />
             <div className="relative w-3/5 flex items-center">
                 <img src={Search} alt="" className="relative left-10 cursor-pointer h-4 w-4" />
                 <input type="search" name="searchbar" id="searchbar" className='border rounded-md border-primary py-3 placeholder:text-sm placeholder:relative placeholder:left-8 text-sm w-full bg-secondary text-dimmestBlack outline outline-1 outline-[#ABBED1] pl-4 focus:outline-[#ABBED1]' placeholder='Search slides by name, category or company'/>
@@ -22,7 +22,7 @@ const DashboardNavbar = () => {
                     <p className='px-[15px] py-2'>A</p>
                 </div>
                 <p>Abiola Oluwaseun</p>
-                <div className="">
+                <div className="cursor-pointer">
                     <img src={notificationbell} alt="notificationbell" />
                 </div>
             </div>
