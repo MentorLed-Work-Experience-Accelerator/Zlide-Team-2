a9cc674c1dab7bf259412d581110b508c09c1e4a
iniubongisemin <eugeneinie@gmail.com>
2024-03-27 09:53:53 +0100

    modify content

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -4,7 +4,7 @@ import { useNavigate  } from 'react-router-dom'
 const Content = () => {
   const navigate = useNavigate()
   return (
-<div className="fixed flex flex-col inset-0 justify-center items-center bg-[#717171] backdrop-blur-2xl z-50">
+<div className="fixed flex flex-col inset-0 justify-center items-center bg-[#717171] backdrop-blur-lg z-50">
       <h1 className="text-black text-start text-3xl font-bold w-[60%] relative top-6">
         Paste your Content
       </h1>
